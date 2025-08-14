ef1c1251f1e1c0816ec5244266a72e18de936075
Siddharth Shahare <148895218+sidshahare@users.noreply.github.com>
2025-08-15 00:57:05 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -15,7 +15,7 @@ export default clerkMiddleware(async (auth, req) => {
   if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
-  }
+  }}
 
   return NextResponse.next();
 });
